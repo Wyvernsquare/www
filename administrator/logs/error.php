1b2dffda483043e29cd030285b6fcfddc73d3046
supasho <kamishiro@ufl.edu>
2017-04-05 02:00:03 +0900

    initial commit of www root

---
+++ administrator/logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2017-04-04 05:22:14 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2017-04-04T05:22:14+00:00	INFO 126.162.20.29	joomlafailure	Username and password do not match or you do not have an account yet.
+2017-04-04T05:50:16+00:00	INFO 126.162.20.29	joomlafailure	The two factor authentication Secret Key is invalid.
